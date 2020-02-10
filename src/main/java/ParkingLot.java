785f46ee3da76e0da3465e53ec188a4b5abdbc8d
Prashant2411 <Prashantbedi241197@gmail.com>
2020-02-10 13:51:18 +0530

    Modified: Test case and code to remove dependency with ParkingLotRepository while testing

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,12 +1,18 @@
 public class ParkingLot {
 
     private Car carDetails;
+    private ParkingLotRepository parkingLotRepository = new ParkingLotRepository();
 
     public ParkingLot(Car carDetails) {
         this.carDetails = carDetails;
     }
 
+    public ParkingLot(Car carDetails, ParkingLotRepository parkingLotRepository) {
+        this.carDetails = carDetails;
+        this.parkingLotRepository = parkingLotRepository;
+    }
+
     public boolean parkVehicle() {
-        return new ParkingLotRepository().getVehicleParked(carDetails);
+        return parkingLotRepository.getVehicleParked(carDetails);
     }
 }
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,26 +1,37 @@
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-import java.text.ParseException;
+import static org.mockito.Mockito.when;
 
 public class ParkingLotTest {
 
     Car carDetails;
 
+    @Mock
+    ParkingLotRepository parkingLotRepository;
+
+    @Rule
+    public MockitoRule mockitoRule = new MockitoJUnit().rule();
+
     @Before
     public void setup() {
-            carDetails = new Car()
-                    .setCarNumber("ABC ")
-                    .setColor("Black")
-                    .setModelName("CarModel")
-                    .setOwnerName("ABC's ")
-                    .getCarDetails();
+        carDetails = new Car()
+                .setCarNumber("ABC ")
+                .setColor("Black")
+                .setModelName("CarModel")
+                .setOwnerName("ABC's ")
+                .getCarDetails();
     }
 
     @Test
     public void whenVehicleArrives_shouldParkInLot() {
-        ParkingLot parkingLot = new ParkingLot(carDetails);
+        ParkingLot parkingLot = new ParkingLot(carDetails, parkingLotRepository);
+        when(parkingLotRepository.getVehicleParked(carDetails)).thenReturn(true);
         boolean carParkStatus = parkingLot.parkVehicle();
         Assert.assertTrue(carParkStatus);
     }
