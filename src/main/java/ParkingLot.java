7979786559dcb45628f0a80a93ad064a8e5596b2
Prashant2411 <Prashantbedi241197@gmail.com>
2020-02-10 17:26:12 +0530

    Refactored: Changed initial assigned value of parkingLotStatus

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -2,7 +2,7 @@ public class ParkingLot {
 
     private Car[] carDetails;
     private ParkingLotRepository parkingLotRepository = new ParkingLotRepository();
-    public static String parkingLotStatus = "HALF";
+    public static String parkingLotStatus = "NOT FULL";
 
     public ParkingLot(Car[] carDetails) {
         this.carDetails = carDetails;
